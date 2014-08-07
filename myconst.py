bb9383f281ad9ff8f3a18c7eac7da2028cbeeb9e
asmuyi <asmuyi@gmail.com>
2014-08-07 00:25:57 -0500

    add myconst.py

---
+++ myconst.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  9 20:25:42 2014
+
+@author: asmuyi 
+"""
+import numpy as np
+
+eps0=8.85e-12
+mu0=4*np.pi*1e-7
+c0=1/np.sqrt(mu0*eps0)
+nm=1e-9
+um=1e-6
+mm=1e-3
+cm=1e-2
+dm=1e-1
+m=1
