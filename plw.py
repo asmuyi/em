8652ccfc47c69f3af135c5a4e8f2a229576699b6
asmuyi <asmuyi@gmail.com>
2014-08-09 22:26:20 -0500

    add tmrefl3.py and plw.py

---
+++ plw.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Example with sp-lu
+
+@author: asmuyi
+"""
+
+import numpy as np
+import getmedia3 as gm3
+import myconst as mc
+import tmrefl3 as tr3
+import matplotlib.pyplot as plt
+#from matplotlib.backends.backend_pdf import PdfPages
+
+""" Change media name and specify the inc angle range and polarization"""
+media='sp-lu'
+tin=np.linspace(0,90,901)
+t0=tin
+pol='TM'
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+refl,tran,v=(np.zeros(len(tin),dtype=complex) for _ in xrange(3))
+ri,tn,n,lam=gm3.getmedia(media)
+for ind in xrange(len(tin)):
+    refl[ind]=tr3.getrefl(ri,tn,n,lam,tin[ind],pol)
+    tran[ind]=tr3.gettran(ri,tn,n,lam,tin[ind],pol)
+x=t0
+ytm = refl;
+
+fig=plt.figure()
+plt.plot(x,ytm,'-*')
+plt.xlabel('angle',fontsize=12)
+plt.ylabel('refl',fontsize=12)
+plt.legend(['model (tmm)'],loc='upper left')
+plt.ylim(0.0,1.0)
+plt.grid(color='r',linestyle='-.')
+plt.title('sp-lumerical')
+fig.set_size_inches(5,4)
+
---
+++ tmrefl3.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+tmrefl.py, calculate the reflection coeff using tmm
+
+Created on Mon Jun  9 21:22:58 2014
+
+@author: Josh
+"""
+
+import numpy as np
+import getneff3 as gn3
+import myconst as mc
+import getz03 as gz3
+
+
+def getr(ri,tn,n,lam,ia,pol):
+    rin,neff=gn3.getneff(ri,tn,n,lam,ia)
+    print rin,neff
+    k=2*np.pi/lam
+    ncos,y,z,zl,rr,r,fi=(np.zeros(n,np.complex) for _ in xrange(7)) 
+    """ zl is impedance on the left boundary
+    rr is refl coeff on the right boundary
+    """
+    for ind in xrange(n):
+        ncos[ind],y[ind],z[ind]=gz3.getncos(ri[ind],pol,ia,rin)
+        fi[ind]=k*ncos[ind]*tn[ind]
+#    print ncos    
+    for ind in xrange(n):
+        if ind==0:
+            rr[-ind-1]=0
+            r[-ind-1]=0
+            zl[-ind-1]=z[-ind-1]
+        else:
+            rr[-ind-1]=(zl[-ind]-z[-ind-1])/(zl[-ind]+z[-ind-1])
+            r[-ind-1]=rr[-ind-1]*np.exp(-1j*2*fi[-ind-1])
+            zl[-ind-1]=z[-ind-1]*(zl[-ind]*np.cos(fi[-ind-1])+1j*z[-ind-1]*np.sin(fi[-ind-1])) \
+                        /(z[-ind-1]*np.cos(fi[-ind-1])+1j*zl[-ind]*np.sin(fi[-ind-1]))
+    return r,rr,fi,z
+    
+def getrefl(ri,tn,n,lam,ia,pol):
+    r,rr,fi,z=getr(ri,tn,n,lam,ia,pol)
+    return np.absolute(rr[0])**2
+#    return rr[0]
+    
+def getreflc(ri,tn,n,lam,ia,pol):
+    r,rr,fi,z=getr(ri,tn,n,lam,ia,pol)
+    return rr[0]
+     
+def getv(ri,tn,n,lam,ia,pol):
+    """ get the v or field value on boundary"""
+    r,rr,fi,z=getr(ri,tn,n,lam,ia,pol)
+    v,vin = ((np.zeros(len(r),np.complex)) for _ in xrange(2))
+    vin[0]=1.0
+    for ind in xrange(len(r)):
+        v[ind]=vin[ind]*(1+rr[ind])*np.exp(-1j*fi[ind])
+        if ind==len(r)-1:
+            break            
+        else:
+            vin[ind+1]=v[ind]/(1+r[ind+1])
+    tran=np.absolute(vin[-1])**2/np.absolute(vin[0])**2/np.conj(z[-1])*np.conj(z[0])
+    return tran,vin,v,z
+    
+def gettran(ri,tn,n,lam,ia,pol):
+    tran,vin,v,z=getv(ri,tn,n,lam,ia,pol)
+    return tran
+    
+
