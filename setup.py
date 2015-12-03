15be38614375976ff0adadc25a9fcf1fd57357d9
asmuyi <asmuyi@gmail.com>
2015-12-03 17:52:10 -0600

    setup and tools change

--- setup.py
+++ setup.py
@@ -2,23 +2,34 @@
 
 """
 
-import os,sys
+import os,sys,subprocess
 from setuptools import setup
 
 def update_path():
     python_path = sys.path
     current_path = os.getcwd()
+    current_platform = sys.platform
+
+    print python_path
+
     if current_path not in python_path:
-        sys.path.insert(0,current_path)
+	if "win" in current_platform:
+	    print "Adding current directory to PYTHONPATH..."
+	    subprocess.call(["set","PYTHONPATH="+
+		"%PYTHONPATH%;"+current_path],shell=True)
+	    subprocess.check_output(["echo","%PYTHONPATH%"],shell=True)
+	    print sys.path
+	else:
+	    sys.path.insert(0,current_path)m
 
 	setup(
 		name = "EM package",
 		version = "0.1",
 		author = "asmuyi",
 		author_email = "asmuyi@gmail.com",
-		description = ("A collection of EM related Python routines for "
-			"simple structures"),
-        url = "https://github.com/asmuyi/em",
+		description = ("A collection of EM related" 
+			"Python routines for simple structures"),
+		url = "https://github.com/asmuyi/em",
 		long_description=read('README'),
 		classifiers = [
 		"Development Status :: 2 - pre-Alpha",
