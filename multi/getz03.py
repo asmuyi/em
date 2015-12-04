75c251bef51946cecb6ce6442720e78eebb9ff56
asmuyi <asmuyi@gmail.com>
2015-12-04 17:38:49 -0600

    add main, import not resolved yet

--- multi/getz03.py
+++ multi/getz03.py
@@ -1,14 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Calculate ncos, y, z of each layer.
-Media info from getneff3.
+"""Get Z0 impedance of multilayer
+Use ncosine as the gauge to get neff and impedance z or reactance y.
 
 """
+from __future__ import absolute_import
+
+import sys
 import numpy as np
 import tools.myconst as mc
-import getneff3 as gn3
-import getmedia3 as gm3
-from tools.myconst import eps0,mu0,c0
 
 def getncos(ri,pol,ia,rin):
     """ Obtain index*cos(beam_angle) and return impedance of each layer.
@@ -26,7 +24,6 @@ def getncos(ri,pol,ia,rin):
     """
     neff = rin * np.sin(ia/180.0*np.pi)
     if ri[0]==ri[2]:
-        print 'isotropic'
         ncos2=ri[0]**2-neff**2
         if ncos2.real<0 and ncos2.imag==0:
             ncos = -np.sqrt(-ncos2)*1j
@@ -37,7 +34,7 @@ def getncos(ri,pol,ia,rin):
         else:
             y=ncos/mc.c0/mc.mu0
     else:
-        print 'uniax'
+    #	anisotropic
         if pol=='TM':
             cos2=ri[0]**2*(ri[2]**2-neff**2)/ \
                 ((neff*ri[2])**2+(ri[0]*ri[2])**2-(ri[0]*neff)**2)
@@ -55,8 +52,32 @@ def getncos(ri,pol,ia,rin):
         else:
             y=ncos/mc.c0/mc.mu0
     z=1/y
-#    print ncos
     return ncos,y,z
+
+def getzin(rin,pol,ia):
+    """ Get the incident layer's impedance. Important for normalization.
+
+    :param ri: refractive index
+    :type ri: complex 
+    :param pol: Polarization
+    :type pol: str
+    :param ia: incident angle
+    :type ia: float in deg
+    :return: impedance (z) 
     
-    if __name__ == "__main__":
-        print "Running getz03"
+    """
+    neff = rin * np.sin(ia/180.0*np.pi)
+    ncos2=rin**2-neff**2
+    if ncos2.real<0 and ncos2.imag==0:
+        ncos = -np.sqrt(-ncos2)*1j
+    else:
+        ncos = np.sqrt(ncos2)
+    if pol=='TM':
+        y=mc.c0*rin**2*mc.eps0/ncos
+    else:
+        y=ncos/mc.c0/mc.mu0
+    return 1./y
+
+if __name__ == "__main__":
+    print "The impedance of the first layer is\n"
+    print getzin(sys.argv[1],sys.argv[2],str(sys.argv[3]))/mc.z0
